3859f0483b2e0736c9cf33f6748aadf6568519ef
Fabon4ick <danil_shipitsyn_00@mail.ru>
2025-06-09 11:08:51 +0300

    3

--- DemonstrationLibrary/DemonstrationLibrary.cs
+++ DemonstrationLibrary/DemonstrationLibrary.cs
@@ -7,33 +7,32 @@ using MosaicLibrary;
 
 namespace DemonstrationLibrary
 {
-    internal class DemonstrationLibrary
+    public class DemonstrationProgram
     {
         private readonly static ClassLibrary c = new ClassLibrary();
 
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             Console.Write("Введите id типа продукции: ");
             int productTypeId = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Введите id типа материала: ");
+            Console.Write("Введите id типа материала: ");
             int materialTypeId = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Введите количество необходимой продукции: ");
+            Console.Write("Введите количество необходимой продукции: ");
             int productAmount = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Введите количесвто материала на складе: ");
+            Console.Write("Введите количество продукции на складе: ");
             int stockAmount = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Введите параметр 1: ");
-            double param1 = double.Parse(Console.ReadLine());
+            Console.Write("Введите параметр 1: ");
+            int param1 = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Введите параметр 2: ");
-            double param2 = double.Parse(Console.ReadLine());
+            Console.Write("Введите параметр 2: ");
+            int param2 = int.Parse(Console.ReadLine());
 
             int result = c.CalculateMaterial(productTypeId, materialTypeId, productAmount, stockAmount, param1, param2);
-            Console.WriteLine($"Необходимо материала: {result}");
+            Console.Write($"Необходимо материала: {result}");
         }
-
     }
 }
